3c5d42f46dcf1bbaa0255fd62bc98ce60261c441
AnastasiaLutkova15 <125673167+AnastasiaLutkova15@users.noreply.github.com>
2023-04-10 12:44:20 +0400

    Программа (Visual Studio Code)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+string[] array = new string[] { "hello", "2", "world", ":-)" };
+string[] shortStrings = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        shortStrings[i] = array[i];
+    }
+    Console.Write($"{shortStrings[i] + " "}");
+}
